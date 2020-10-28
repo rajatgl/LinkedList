495da54698e5be581373837c9aaa2c2b918d7efd
Rajat Gundi <glrajat@gmail.com>
2020-10-28 10:56:34 +0530

    Created: LinkedList of Numbers

---
+++ src/main/java/LinkedList/Linked_List.java
@@ -0,0 +1,13 @@
+package LinkedList;
+//Import the LinkedList class
+import java.util.LinkedList;
+public class Linked_List {
+	public static void main(String[] args)
+	{
+	    LinkedList<Integer> listOfNumbers = new LinkedList<Integer>();
+	    listOfNumbers.add(56);
+	    listOfNumbers.add(30);
+	    listOfNumbers.add(70);
+	    System.out.println(listOfNumbers);
+	}
+}
